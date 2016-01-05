271f604e3146d8d4557fa31949b72d06b96991bf
Soldras <apenalver@openstudio.fr>
2016-01-05 14:41:33 +0100

    fix delete Person

---
+++ Controller/PersonController.php
@@ -0,0 +1,36 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+/*************************************************************************************/
+
+namespace DealerTeam\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Thelia\Tools\URL;
+
+/**
+ * Class PersonController
+ * @package DealerTeam\Controller
+ */
+class PersonController extends \Team\Controller\PersonController
+{
+    /**
+     * @inheritDoc
+     */
+    protected function redirectToListTemplate()
+    {
+        $id = $this->getRequest()->query->get("dealer_id");
+        if(null === $id){
+            $id = $this->getRequest()->request->get("dealer_id");
+        }
+        return new RedirectResponse(URL::getInstance()->absoluteUrl("/admin/module/Dealer/dealer/edit#team",["dealer_id" => $id]));
+    }
+}
